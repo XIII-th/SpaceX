b63386661089d4e44812af2f213b8cceec08f931
Sergey Bubenshchikov <Sergey.V.Bubenshchikov@gmail.com>
2022-03-07 16:22:52 +0500

    Hilt DI added to the project

---
+++ app/src/main/java/com/xiii_lab/spacex/SpaceXApplication.kt
@@ -0,0 +1,10 @@
+package com.xiii_lab.spacex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by XIII-th on 07.03.2022
+ */
+@HiltAndroidApp
+internal class SpaceXApplication : Application()
